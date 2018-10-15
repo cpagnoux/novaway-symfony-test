e7fbaeebcaa58168df048f82fa1d7b28ff8b086f
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2018-10-15 18:42:24 +0200

    Add entities

---
+++ src/Entity/DVD.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DVDRepository")
+ */
+class DVD
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=33)
+     */
+    private $isan;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Director", inversedBy="dvds")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $director;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Actor", inversedBy="dvds")
+     */
+    private $actors;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $releaseDate;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $storyline;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=2, nullable=true)
+     */
+    private $price;
+
+    public function __construct()
+    {
+        $this->actors = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsan(): ?string
+    {
+        return $this->isan;
+    }
+
+    public function setIsan(string $isan): self
+    {
+        $this->isan = $isan;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDirector(): ?Director
+    {
+        return $this->director;
+    }
+
+    public function setDirector(?Director $director): self
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Actor[]
+     */
+    public function getActors(): Collection
+    {
+        return $this->actors;
+    }
+
+    public function addActor(Actor $actor): self
+    {
+        if (!$this->actors->contains($actor)) {
+            $this->actors[] = $actor;
+        }
+
+        return $this;
+    }
+
+    public function removeActor(Actor $actor): self
+    {
+        if ($this->actors->contains($actor)) {
+            $this->actors->removeElement($actor);
+        }
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $releaseDate): self
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getStoryline(): ?string
+    {
+        return $this->storyline;
+    }
+
+    public function setStoryline(?string $storyline): self
+    {
+        $this->storyline = $storyline;
+
+        return $this;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Director.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DirectorRepository")
+ */
+class Director
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\DVD", mappedBy="director", orphanRemoval=true)
+     */
+    private $dvds;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Bluray", mappedBy="director", orphanRemoval=true)
+     */
+    private $blurays;
+
+    public function __construct()
+    {
+        $this->dvds = new ArrayCollection();
+        $this->blurays = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|DVD[]
+     */
+    public function getDvds(): Collection
+    {
+        return $this->dvds;
+    }
+
+    public function addDvd(DVD $dvd): self
+    {
+        if (!$this->dvds->contains($dvd)) {
+            $this->dvds[] = $dvd;
+            $dvd->setDirector($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDvd(DVD $dvd): self
+    {
+        if ($this->dvds->contains($dvd)) {
+            $this->dvds->removeElement($dvd);
+            // set the owning side to null (unless already changed)
+            if ($dvd->getDirector() === $this) {
+                $dvd->setDirector(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Bluray[]
+     */
+    public function getBlurays(): Collection
+    {
+        return $this->blurays;
+    }
+
+    public function addBluray(Bluray $bluray): self
+    {
+        if (!$this->blurays->contains($bluray)) {
+            $this->blurays[] = $bluray;
+            $bluray->setDirector($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBluray(Bluray $bluray): self
+    {
+        if ($this->blurays->contains($bluray)) {
+            $this->blurays->removeElement($bluray);
+            // set the owning side to null (unless already changed)
+            if ($bluray->getDirector() === $this) {
+                $bluray->setDirector(null);
+            }
+        }
+
+        return $this;
+    }
+}
