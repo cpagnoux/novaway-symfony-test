9d6f40fbdb687b40503e07c9b2bffceb3c1f7d38
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2018-10-18 01:32:44 +0200

    Add search page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\SearchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -14,6 +16,19 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(Request $request, SearchRepository $searchRepository)
+    {
+        $results = $searchRepository->search($request->get('term'));
+
+        return $this->render('default/search.html.twig', [
+            'search_term' => $request->get('term'),
+            'results' => $results,
         ]);
     }
 }
